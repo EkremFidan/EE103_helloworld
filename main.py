63a688cc8e6a1fb418aaae443ef27bc5b1d5b744
ekrem <eko.1.fidan@gmail.com>
2025-10-08 15:15:07 +0300

    Add main.py, .gitignore, and README.md

---
+++ main.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def say_hello():
+    
+    
+
+    print("Hello World from EE103!")
+
+
+
+    
+
+if __name__ == "__main__":
+    
+    say_hello()
+
+    print(f"Using NumPy version: {np.__version__}")
+
+    arr = np.array([1, 2, 3])
+    print("NumPy works. Sum =", arr.sum())
